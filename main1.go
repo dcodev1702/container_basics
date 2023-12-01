46260439ea0b8f67c0b4e98b573e7f77362d76ca
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-12-01 00:27:45 -0500

    Update main1.go

--- main1.go
+++ main1.go
@@ -3,66 +3,62 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"syscall"
+  "fmt"
+  "os"
+  "os/exec"
+  "syscall"
 )
 
-
 func main() {
-	switch os.Args[1] {
-	  case "run":
-		  run()
-	  case "child":
-		  child()
-	  default:
-		  panic("I'm confused!")
-	}
+   switch os.Args[1] {
+     case "run":
+       run()
+     case "child":
+       child()
+     default:
+       panic("I'm confused!")
+   }
 }
-
 
 func run() {
-	fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
+   fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
 
-	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-	cmd.Stdin  = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr {
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNS | syscall.CLONE_NEWPID,
-		Credential: &syscall.Credential {Uid: 0, Gid: 0},
-    UidMappings: []syscall.SysProcIDMap {
-			{ ContainerID: 0, HostID: os.Getuid(), Size: 1, },
-		},
-		GidMappings: []syscall.SysProcIDMap {
-			{ ContainerID: 0, HostID: os.Getgid(), Size: 1, },
-		},
-	}
-	must(cmd.Run())
+   cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+   cmd.Stdin  = os.Stdin
+   cmd.Stdout = os.Stdout
+   cmd.Stderr = os.Stderr
+   cmd.SysProcAttr = &syscall.SysProcAttr {
+       Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNS | syscall.CLONE_NEWPID,
+       Credential: &syscall.Credential {Uid: 0, Gid: 0},
+       UidMappings: []syscall.SysProcIDMap {
+	  { ContainerID: 0, HostID: os.Getuid(), Size: 1, },
+       },
+       GidMappings: []syscall.SysProcIDMap {
+	  { ContainerID: 0, HostID: os.Getgid(), Size: 1, },
+       },
+   }
+   must(cmd.Run())
 }
-
 
 func child() {
-	fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
+   fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
 
-  must(syscall.Sethostname([]byte("container")))
-	must(syscall.Chroot("/home/lorenzo/container_basics/alpinefs"))
-	must(os.Chdir("/"))
-	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+   must(syscall.Sethostname([]byte("container")))
+   must(syscall.Chroot("/home/lorenzo/container_basics/alpinefs"))
+   must(os.Chdir("/"))
+   must(syscall.Mount("proc", "proc", "proc", 0, ""))
 	
-  cmd := exec.Command(os.Args[2], os.Args[3:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+   cmd := exec.Command(os.Args[2], os.Args[3:]...)
+   cmd.Stdin = os.Stdin
+   cmd.Stdout = os.Stdout
+   cmd.Stderr = os.Stderr
 	
-  must(cmd.Run())
-	must(syscall.Unmount("proc", 0))
+   must(cmd.Run())
+   must(syscall.Unmount("proc", 0))
 }
 
-
 func must(err error) {
-	if err != nil {
-		panic(err)
-	}
+   if err != nil {
+      panic(err)
+   }
 }
